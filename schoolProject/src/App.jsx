32bedfb3da8dbc4e3598344d1caebb7db192083d
Clint <clinthiles1992@gmail.com>
2025-03-03 12:52:48 -0800

    Refactor App component to simplify routing structure and prepare for future page integration

--- schoolProject/src/App.jsx
+++ schoolProject/src/App.jsx
@@ -1,31 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// import Home from "./pages/Home";
-// import About from "./pages/About";
-// import Admission from "./pages/Admission";
-// import Academic from "./pages/Academic";
-// import Sports from "./pages/Sports";
-// import Contribute from "./pages/Contribute";
-// import Contact from "./pages/Contact";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import HomePage from "../src/pages/HomePage";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Navbar />}>
-          {/* <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="admission" element={<Admission />} />
-          <Route path="academic" element={<Academic />} />
-          <Route path="sports" element={<Sports />} />
-          <Route path="contribute" element={<Contribute />} />
-          <Route path="contact" element={<Contact />} /> */}
-        </Route>
-      </Routes>
+      <Navbar />
+      <Routes></Routes>
     </Router>
   );
 }
 
 export default App;
+{
+  /* <Route index element={<Home />} />
+<Route path="about" element={<About />} />
+<Route path="admission" element={<Admission />} />
+<Route path="academic" element={<Academic />} />
+<Route path="sports" element={<Sports />} />
+<Route path="contribute" element={<Contribute />} />
+<Route path="contact" element={<Contact />} />  */
+}
