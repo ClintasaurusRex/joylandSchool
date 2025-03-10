d9e2f687602686eb9518eb6baf0c9b9a59206a5f
Clint <clinthiles1992@gmail.com>
2025-03-10 10:56:10 -0700

    setting up the config file

--- schoolProject/src/utils/config.js
+++ schoolProject/src/utils/config.js
@@ -3,7 +3,27 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'APIKEY',
-  authDomain: 'firbaseProjectId.firebaseapp.com',
+  apiKey: 'AIzaSyD8TirGcX2_0X6Ox4dM7O_V_2v2G48MBm4',
+  authDomain: 'joyland-prime-academy.firebaseapp.com',
+  projectId: 'joyland-prime-academy',
+  storageBucket: 'joyland-prime-academy.firebasestorage.app',
+  messagingSenderId: '491968680811',
+  appId: '1:491968680811:web:f5cc03b4def019ebb4f22e',
 };
+
+// Init Firebase :)
+const app = initializeApp(firebaseConfig);
+
+// Firebase services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+export default app;
+
+// project unique id = joyland-prime-academy
