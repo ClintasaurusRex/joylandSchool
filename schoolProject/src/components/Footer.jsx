c60cd8c4542001a6c035c7ecb9b380a06a6c4662
Clint <clinthiles1992@gmail.com>
2025-03-12 15:55:09 -0700

    style: standardize quotes and improve Footer component styles

--- schoolProject/src/components/Footer.jsx
+++ schoolProject/src/components/Footer.jsx
@@ -1,82 +1,78 @@
-import { Box, Container, Typography, Link } from '@mui/material';
-import { Link as RouterLink } from 'react-router-dom';
+import { Box, Container, Typography, Link } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
 
-import '../styles/Footer.scss';
+import "../styles/Footer.scss";
 
 const Footer = () => {
   return (
     <Box
-      component='footer'
+      component="footer"
       sx={{
-        backgroundColor: '#2f27ce',
-        color: 'white',
+        backgroundColor: "#2f27ce",
+        color: "white",
         py: 3,
-        mt: '0',
+        mt: "auto",
         mb: 0,
       }}
     >
-      <Container maxWidth='xl'>
-        <Typography
-          varient='h6'
-          align='center'
-          sx={{ mb: 2, fontWeight: 'bold' }}
-        >
+      <Container maxWidth="xl">
+        <Typography variant="h6" align="center" sx={{ mb: 2, fontWeight: "bold" }}>
           Quick Links
         </Typography>
-        <Box sx={{ display: 'flex', justifyContent: 'center', mb: 3 }}>
+        <Box sx={{ display: "flex", justifyContent: "center", mb: 3 }}>
           <Link
-            className='footer-link'
+            className="footer-link"
             component={RouterLink}
-            to='/'
-            color='inherit'
-            style={{ margin: '0 20px', textDecoration: 'none' }}
+            to="/"
+            color="inherit"
+            style={{ margin: "0 20px", textDecoration: "none" }}
           >
             Home
           </Link>
           <Link
-            className='footer-link'
+            className="footer-link"
             component={RouterLink}
-            to='/academic'
-            color='inherit'
-            sx={{ mx: 5, textDecoration: 'none' }}
+            to="/academic"
+            color="inherit"
+            sx={{ mx: 5, textDecoration: "none" }}
           >
             Academic
           </Link>
           <Link
-            className='footer-link'
+            className="footer-link"
             component={RouterLink}
-            to='/admission'
-            color='inherit'
-            sx={{ mx: 5, textDecoration: 'none' }}
+            to="/admission"
+            color="inherit"
+            sx={{ mx: 5, textDecoration: "none" }}
           >
             Admission
           </Link>
           <Link
-            className='footer-link'
+            className="footer-link"
             component={RouterLink}
-            to='/contribute'
-            color='inherit'
-            sx={{ mx: 5, textDecoration: 'none' }}
+            to="/contribute"
+            color="inherit"
+            sx={{ mx: 5, textDecoration: "none" }}
           >
             Contribute
           </Link>
         </Box>
-        <Box sx={{ display: 'flex', justifyContent: 'center', mb: 2 }}>
+        <Box sx={{ display: "flex", justifyContent: "center", mb: 2 }}>
           <Link
-            className='footer-link'
+            className="footer-link"
             component={RouterLink}
-            to='/contact'
-            color='inherit'
-            sx={{ mx: 5, textDecoration: 'none' }}
+            to="/contact"
+            color="inherit"
+            sx={{ mx: 5, textDecoration: "none" }}
           >
             Contact Us
           </Link>
           <Link
-            className='footer-link'
+            className="footer-link"
             component={RouterLink}
-            to='admin/login'
-            color='inherit'
-            sx={{ mx: 5, textDecoration: 'none' }}
+            to="admin/login"
+            color="inherit"
+            sx={{ mx: 5, textDecoration: "none" }}
           >
             Login
           </Link>
@@ -90,14 +86,14 @@ const Footer = () => {
             Admin Sign Up
           </Link> */}
         </Box>
-        <Typography variant='body2' align='center'>
+        <Typography variant="body2" align="center">
           © {new Date().getFullYear()} Joyland Prime Academy.
           <img
-            src='/joylandschool.png'
-            alt='joyland picture'
-            height='25px'
-            width='25px'
-            style={{ marginLeft: '10px' }}
+            src="/joylandschool.png"
+            alt="joyland picture"
+            height="25px"
+            width="25px"
+            style={{ marginLeft: "10px" }}
           />
         </Typography>
       </Container>
